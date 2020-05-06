8b0f7294075af5483f848f36fe181cdaeb0e66a0
WillCJR <wcranmer@gmail.com>
2020-05-06 01:15:02 +0000

    Pushing Urban Dictionary for first time.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import requests, os
+import requests, os, json
 
 # Retrieve your API credentials from the .env file
 if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
@@ -6,7 +6,43 @@ if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
     exit(1)
 
 # Get credentials from the .env file
-API_HOST = os.getenv("API_HOST")
+API_HOST = os.getenv("API_HOST") 
 API_KEY = os.getenv("API_KEY")
 
 # continue with your application here
+
+def sync_word(user_response,_permission):
+    text_file = open("src/"+user_response+"_definition.txt",_permission)
+    if _permission =="w+": 
+        text_file.write(body["list"][0]["definition"])
+    else:
+        return text_file.read()
+    text_file.close()
+
+user_response = input("What term do you want to look for?")
+
+if os.path.isfile("src/"+user_response+"_definition.txt"):
+    print("Fetching From Cache")
+    print(sync_word(user_response,"r"))
+else:
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+user_response
+
+    headers = {
+        'x-rapidapi-host': API_HOST,
+        'x-rapidapi-key': API_KEY
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    body = response.json()
+
+    definitions_array = body["list"]
+
+    single_definition_from_dict = definitions_array[0]
+
+    print(single_definition_from_dict)
+    #print(response.text)
+
+    text_file = open("src/"+user_response+"_definition.txt", "w")
+    text_file.write(single_definition_from_dict)
+    text_file.close()
